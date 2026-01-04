223dcf326a1ead5fe4322e8bdbd22ed61ed58909
Guilherme Andrade <g@gandrade.net>
2026-01-04 17:48:18 +0000

    Sets: run formatter

--- src/b5_sets_node.erl
+++ src/b5_sets_node.erl
@@ -294,7 +294,8 @@
 
 %%
 
--define(NODE_CHECK_ENABLED, false). % defined(TEST)).
+% defined(TEST)).
+-define(NODE_CHECK_ENABLED, false).
 
 -if(?NODE_CHECK_ENABLED).
 -define(CHECK_NODE(Node), check_node(?LINE, Node)).
